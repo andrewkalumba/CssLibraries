f5592dc244d4433de3ce8d5eba2a3190fbbf945c
andrew <andrewkalumba29@gmail.com>
2025-06-12 10:31:59 +0200

    added neww link

--- src/app/sass/page.tsx
+++ src/app/sass/page.tsx
@@ -2,7 +2,8 @@
 import Styling from "@/components/Styling"
 const SASS = () => {
     return (
-        <Styling h1={"Sass"} github="git@github.com:andrewkalumba/healthcare.git" vercel="https://healthcare-95hvznp2t-andrewkalumbas-projects.vercel.app" />
+        <Styling h1={"Sass"} github="git@github.com:andrewkalumba/healthcare.git" 
+        vercel="http://healthcare-qa4ylx7fv-andrewkalumbas-projects.vercel.app"/>
     )
 }
 export default SASS
